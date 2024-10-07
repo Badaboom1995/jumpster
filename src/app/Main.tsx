d2becf1852ff2c4b8cc212cb13d40d0349cd986b
Badavoo <lordalurard@gmail.com>
2024-10-08 00:08:52 +0800

    fix

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,20 +1,20 @@
 'use client'
-import React, {useEffect} from 'react';
-import {useLaunchParams} from "@telegram-apps/sdk-react";
-import { createClient } from '@supabase/supabase-js'
+import React from 'react';
+// import {useLaunchParams} from "@telegram-apps/sdk-react";
+// import { createClient } from '@supabase/supabase-js'
 import VideoCanvas from "@/components/JumpFlow/TestFlow";
-const supabase = createClient('https://adrdxahjylqbmxomhrmi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcmR4YWhqeWxxYm14b21ocm1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2Njc1NDAsImV4cCI6MjA0MzI0MzU0MH0.pe1KulD4qwauzZxD0PFIV0cfdnuVii12tdgUHsQsRiA')
+// const supabase = createClient('https://adrdxahjylqbmxomhrmi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcmR4YWhqeWxxYm14b21ocm1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2Njc1NDAsImV4cCI6MjA0MzI0MzU0MH0.pe1KulD4qwauzZxD0PFIV0cfdnuVii12tdgUHsQsRiA')
 
 const Main = () => {
-    const lp = useLaunchParams();
+    // const lp = useLaunchParams();
 
-    useEffect(() => {
-        supabase
-            .from('Users')
-            .select('*')
-            .eq('telegram_id', lp.initData?.user?.id)
-            .then(console.log)
-    }, []);
+    // useEffect(() => {
+    //     supabase
+    //         .from('Users')
+    //         .select('*')
+    //         .eq('telegram_id', lp.initData?.user?.id)
+    //         .then(console.log)
+    // }, []);
 
     return (
         <div className='w-[100vw] h-[100vh] bg-white'>
