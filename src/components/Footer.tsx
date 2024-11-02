3c8c3c46ad61cd847a255fec15eba49498ea7571
Badavoo <lordalurard@gmail.com>
2024-11-03 00:12:52 +0800

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -42,7 +42,7 @@ const Footer = () => {
   };
   return (
     <div
-      className={`fixed bottom-[0px] left-1/2 z-10 w-full max-w-[500px] -translate-x-1/2 bg-background-dark px-[12px] py-[8px] pb-[12px] ${!visible && "hidden"}`}
+      className={`fixed bottom-[8px] left-1/2 z-10 w-full max-w-[500px] -translate-x-1/2 bg-background-dark px-[12px] py-[8px] pb-[12px] ${!visible && "hidden"}`}
     >
       <ul className="flex w-full justify-between">
         <Navitem
